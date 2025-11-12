3b2446e38e6135558f8cd0ba573e0d8dc1568745
blankony <56399492+blankony@users.noreply.github.com>
2025-11-13 00:27:11 +0700

    setup provider package

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,10 +1,15 @@
+import 'package:brew_crew/models/user.dart';
 import 'package:myfirebase/screens/authenticate/authenticate.dart';
 import 'package:myfirebase/screens/home/home.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+
+    final user = Provider.of<User>(context);
+    print(user);
     
     // return either the Home or Authenticate widget
     return Authenticate();
